341f94b083999c04e222951d9acb9a26eb906cf3
Ram-blip <ramcruze2000@gmail.com>
2024-08-20 16:47:19 +0530

    Day 2 -  Arranging the post, post detail, post author
    Designed post, post author, post details and main dashboard which shows all the post.

---
+++ client/src/data.js
@@ -0,0 +1,39 @@
+import Thumbnail1 from '../src/images/blog1.jpg';
+import Thumbnail2 from '../src/images/blog2.jpg';
+import Thumbnail3 from '../src/images/blog3.jpg';
+import Thumbnail4 from '../src/images/blog4.jpg';
+
+export const DUMMY_POSTS = [
+    {
+        id: '1',
+        thumbnail: Thumbnail1,
+        category: 'education',
+        title: 'This is the title of the very first post on this blog.',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor.',
+        authorID: 3
+    },
+    {
+        id: '2',
+        thumbnail: Thumbnail2,
+        category: 'science',
+        title: 'Welcome to Ghana',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor.',
+        authorID: 1
+    },
+    {
+        id: '3',
+        thumbnail: Thumbnail3,
+        category: 'weather',
+        title: 'This is the title of the very third post on this blog.',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor.',
+        authorID: 13
+    },
+    {
+        id: '4',
+        thumbnail: Thumbnail4,
+        category: 'farming',
+        title: 'This is the title of the very last post on this blog.',
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat dolor dolor.',
+        authorID: 11
+    }
+];
