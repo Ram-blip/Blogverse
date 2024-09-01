c488a216871c28546c8d56419e78b9083dae5a30
Ram-blip <ramcruze2000@gmail.com>
2024-09-01 19:22:02 +0530

    Created backend for registering users
    Used postman, created one api end point, created middleware and handled error, also finished the registering user.

---
+++ server/controllers/userController.js
@@ -0,0 +1,100 @@
+const bcrypt = require('bcryptjs')
+
+const User = require('../models/userModel')
+const HttpError = require("../models/errorModel")
+
+
+// ======================= REGISTER A NEW USER
+// POST: api/users/register
+// UNPROTECTED
+const registerUser = async (req, res, next) => {
+    try {
+        const {name, email, password, password2} = req.body;
+        if(!name || !email || !password) {
+            return next(new HttpError("fill in all fields.", 422))
+        }
+
+        const newEmail = email.toLowerCase()
+
+        const emailExists = await User.findOne({email: newEmail})
+        if(emailExists) {
+            return next(new HttpError("Email already Exists.", 422))
+        }
+
+        if((password.trim()).length < 6){
+            return next(new HttpError("Passoword Should be atleast 6 characters", 422))
+        }
+
+        if(password != password2){
+            return next(new HttpError("Password do not match.", 422))
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedpass = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({name, email : newEmail, password: hashedpass})
+        res.status(201).json(`New user ${newUser.email} registered`)
+    } catch (error) {
+        return next(new HttpError("User Registration failed.", 422))
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ======================= LOGIN A REGISTERED USER
+// POST: api/users/login
+// UNPROTECTED
+const loginUser = async (req, res, next) => {
+    res.json("Login User")
+}
+
+// ======================= USER PROFILE
+// POST: api/users/:id
+// PROTECTED
+const getUser = async(req, res, next) => {
+    res.json("User Profile")
+}
+
+// ======================= CHANGE USER AVATAR
+// POST: api/users/change-avatar
+// PROTECTED
+const changeAvatar = async(req, res, next) => {
+    res.json("change User Avatar")
+}
+
+// ======================= EDIT USER DETAILS ( from profile)
+// POST: api/users/edit-user
+// PROTECTED
+const editUser = async (req, res, next) => {
+    res.json("Edit user details")
+}
+
+// ======================= GET AUTHORS
+// POST: api/users/authors
+// UNPROTECTED
+const getAuthors =async (req, res, next) => {
+    res.json("Get all users/authors")
+}
+
+module.exports = {registerUser, loginUser, getUser, changeAvatar, editUser, getAuthors}
--- server/index.js
+++ server/index.js
@@ -3,6 +3,20 @@ const cors = require('cors')
 const {connect} = require('mongoose')
 require('dotenv').config()
 
-const app = express();
+const userRoutes = require('./routes/userRoutes')
+const postRoutes = require('./routes/postRoutes')
+const { notFound, Errorhandler } = require('./middleware/errorMiddleware')
 
-app.listen(5001,() => console.log("Server Running on port 5001"))
+const app = express();
+app.use(express.json({extended : true}))
+app.use(express.urlencoded({extended : true}))
+app.use(cors({credentials: true, origin: "http://localhost:3000" }))
+
+app.use('/api/users', userRoutes)
+app.use('/api/posts', postRoutes)
+
+app.use(notFound) 
+app.use(Errorhandler)
+
+
+connect(process.env.MONGO_URI).then(app.listen(5001,() => console.log(`Server Stared on port ${process.env.PORT}`))).catch(error => {console.log(error)})
