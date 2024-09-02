a6878697234780333b9d0fd341573c533056f043
Ram-blip <ramcruze2000@gmail.com>
2024-09-02 22:08:49 +0530

    Done with the get user
    completed get user, auth middleware for the change avatar.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,8 @@
 const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken")
+const fs = require('fs')
+const path = require('path')
+const {v4: uuid} = require('uuid')
 
 const User = require('../models/userModel')
 const HttpError = require("../models/errorModel")
@@ -43,44 +47,103 @@ const registerUser = async (req, res, next) => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // ======================= LOGIN A REGISTERED USER
 // POST: api/users/login
 // UNPROTECTED
 const loginUser = async (req, res, next) => {
-    res.json("Login User")
+    try {
+        const {email, password} = req.body;
+        if(!email || !password) {
+            return next(new HttpError("fill in all fields", 422))
+        }
+
+        const newEmail = email.toLowerCase();
+
+        const user = await User.findOne({email: newEmail})
+        if(!user){
+            return next(new HttpError("Invalid credentials"), 422)
+        }
+
+        const comparepass = await bcrypt.compare(password, user.password)
+        if(!comparepass) {
+            return next(new HttpError("Invalid Credentials",422))
+        }
+
+        const {_id:id, name} = user;
+        const token = jwt.sign({id, name}, process.env.JWT_SECRET, {expiresIn : "1d"})
+
+        res.status(200).json({token, id, name})
+    } catch (error) {
+        return next(new HttpError("Login failed. Please check your credentials", 422))
+    }
 }
+
+
+
+
 
 // ======================= USER PROFILE
 // POST: api/users/:id
 // PROTECTED
 const getUser = async(req, res, next) => {
-    res.json("User Profile")
+    try {
+        const {id} = req.params;
+        const user = await User.findById(id).select('-password');
+        if(!user){
+            return next(new HttpError("User not found", 404))
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ======================= CHANGE USER AVATAR
 // POST: api/users/change-avatar
 // PROTECTED
 const changeAvatar = async(req, res, next) => {
-    res.json("change User Avatar")
+    try {
+        if(!req.files.avatar) {
+            return next(new HttpError("please choose an image",422))
+        }
+
+        // find user from database
+        const user = await User.findById(req.user.id)
+
+        // delete old avatar if exists
+        if(user.avatar) {
+            fs.unlink(path.join(__dirname, '..', 'uploads', user.avatar, (err) => {
+                if(err) {
+                    return next(new HttpError(err))
+                }
+            }))
+        }
+
+        const {avatar} = req.files;
+
+        //check file size
+        if(avatar.size > 500000){
+            return next(new HttpError("profile pic is too big. Should be less than 500kb",422))
+        }
+
+        let fileName;
+        fileName = avatar.name;
+        let splittedFilename = fileName.split('.')
+        let newFilename = splittedFilename[0] + uuid() + '.' + splittedFilename[splittedFilename.length - 1]
+        avatar.mv(path.join(__dirname, '..', 'uploads', newFilename), async (err) =>  {
+            if(err) {
+                return next(new HttpError(err))
+            }
+
+            const updatedAvatar = await User.findByIdAndUpdate(req.user.id, {avatar:newFilename}, {new:true})
+            if(!updatedAvatar) {
+                return next(new HttpError("Avatar couldn't be changed ", 422))
+            }
+            res.status(200).json(updatedAvatar)
+        })
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ======================= EDIT USER DETAILS ( from profile)
@@ -94,7 +157,12 @@ const editUser = async (req, res, next) => {
 // POST: api/users/authors
 // UNPROTECTED
 const getAuthors =async (req, res, next) => {
-    res.json("Get all users/authors")
+    try {
+        const authors = await User.find().select('-password');
+        res.json(authors);
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 module.exports = {registerUser, loginUser, getUser, changeAvatar, editUser, getAuthors}
