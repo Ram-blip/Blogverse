c2e1993503e177d0fd74192f07d721dd74d722b2
Ram-blip <ramcruze2000@gmail.com>
2024-09-04 22:02:28 +0530

    Done with create post
    Done with create post, get post, get all post and get by category

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,46 +1,125 @@
+const Post = require('../models/postModel')
+const User = require('../models/userModel')
+const path = require('path')
+const fs = require('fs')
+const {v4: uuid} = require('uuid')
+const HttpError = require('../models/errorModel')
 
 
 // ================================ CREATE A POST
 // POST: api/posts
 // PROTECTED
 const createPost = async (req, res, next) => {
-    res.json("Create post")
+    try {   
+        let {title, category, description} = req.body;
+        if(!title || !category || !description || !req.files) {
+            return next(new HttpError("fill in all fields and choose thumbnail",422))
+        }
+
+        const {thumbnail} = req.files;
+
+        //check the file size
+        if(thumbnail.size > 2000000){
+            return next(new HttpError("thumbnail is too big, reduce it"))
+        }
+
+        let filename = thumbnail.name;
+        let splittedFilename = filename.split('.')
+        let newFilename = splittedFilename[0] + uuid() + "." + splittedFilename[splittedFilename.length - 1]
+        thumbnail.mv(path.join(__dirname, '..', '/uploads', newFilename), async (err) => {
+            if(err) {
+                return next(new HttpError(err))
+            } else {
+                const newPost = await Post.create({title, category, description, thumbnail: newFilename, creator: req.user.id})
+                if(!newPost) {
+                    return next(new HttpError("post couldnt be created", 422))
+                }
+                // find user and increase post ccount by 1
+                const currentUser = await User.findById(req.user.id)
+                const userPostCount = currentUser.posts + 1;
+                await User.findByIdAndUpdate(req.user.id, {posts: userPostCount })
+
+                res.status(201).json(newPost)
+            }
+        })
+
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
+
+
+
+
+
+
+
+
 
 // ================================ GET ALL POSTS
 // GET: api/posts
 // PROTECTED
 const getPosts = async (req, res, next) => {
-    res.json("Get all posts")
+    try {
+        const posts = await Post.find().sort({updatedAt: -1})
+        res.status(200).json(posts)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ================================ GET SINGLE POST
 // GET: api/posts/:id
 // UNPROTECTED
 const getPost = async (req, res, next) => {
-    res.json("Get Single Post")
+    try {
+        const postId = req.params.id;
+        const post = await Post.findById(postId);
+        if(!post) {
+            return next(new HttpError("Post not found",404))
+        } 
+        res.status(200).json(post)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ================================ GET POSTS BY CATEGORY
 // GET: api/posts/categories/:category
 // UNPROTECTED
 const getCatPosts = async (req, res, next) => {
-    res.json("Get posts by category")
+    try {
+        const {category} = req.params;
+        const catPosts = await Post.find({category}).sort({createdAt: -1})
+        res.status(200).json(catPosts)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 
-// ================================ GET AUTHOR POST
+// ================================ GET USER/AUTHOR POST
 // GET: api/posts/users/:id
 // UNPROTECTED
 const getUserPosts = async (req, res, next) => {
-    res.json("Get user Post")
+    try {
+        const {id} = req.params;
+        const posts = await Post.find({creator: id}).sort({createdAt: -1})
+        res.status(200).json(posts)
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ================================ EDIT POST
 // PATCH: api/posts/:id
 // PROTECTED
 const editPost = async (req, res, next) => {
-    res.json("Edit post")
+    try {
+        res.json("Edit post")
+    } catch (error) {
+        return next(new HttpError(error))
+    }
 }
 
 // ================================ DELETE POST
